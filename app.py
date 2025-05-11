46d7888a8a7637136e940197d98534f36a74ac37
SolCarreras <soledadcarreras910@gmail.com>
2025-05-10 23:20:52 -0300

    Terminando Contacto-Modificaciones

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template
+import pyodbc
+
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
+
+
+# Conexión a SQL Server
+def conexion_bd():
+    return pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-BQLA1M6\\SQLEXPRESS07;DATABASE=MiSitio;Trusted_Connection=yes;')
+
+@app.route('/contacto', methods=['GET', 'POST'])
+def contacto():
+    mensaje = None
+    if request.method == 'POST':
+        nombre_apellido = request.form['nombre_apellido']
+        telefono = request.form['telefono']
+        mensaje_formulario = request.form['mensaje']
+
+        conn = conexion_bd()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO contacto (nombre_apellido, telefono, mensaje)
+            VALUES (?, ?, ?)""",
+            nombre_apellido, telefono, mensaje_formulario)
+        conn.commit()
+        conn.close()
+
+        mensaje = "¡Gracias por contactarte! Pronto nos pondremos en contacto."
+
+    return render_template('contacto.html', mensaje=mensaje)
+
+# Rutas
 @app.route('/')
 def inicio():
     return render_template('index.html')
@@ -10,9 +40,6 @@ def inicio():
 def mis_proyectos():
     return render_template('mis_proyectos.html')
 
-@app.route('/contacto')
-def contacto():
-    return render_template('contacto.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
